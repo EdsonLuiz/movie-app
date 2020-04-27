b582bdf48327a03c9a7c988e480a3126531c4178
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-04-27 15:29:38 -0300

    Fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 
 function App() {
+  const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getMovies = async() => {
+    const data = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    setMovies(data)
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    getMovies()
+  }, [])
+
   return (
     <div>
-      <h1>OK</h1>
+      {isLoading ? <h1>Loading...</h1>
+      :<h1>OK</h1>
+      }
+
     </div>
   );
 }
