c64bc11632a40e197a3ff62cd981c3d18cd1be37
EdsonLuiz <EdsonLuiz@users.noreply.github.com>
2020-04-28 17:54:04 +0000

    deploy: 506dea24be5e4ae45c20dd20fbab2dbe119b87e1

---
+++ precache-manifest.bfa0b72fbf5b1006f607ac326f783bc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "137db2c106648ebecd83f09421e7a530",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56d59e850f3eded7f3a4",
+    "url": "/static/js/2.4fdce9f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4fdce9f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c40dc1c7a29169eb9e8d",
+    "url": "/static/js/main.3140e64b.chunk.js"
+  },
+  {
+    "revision": "9b4741d2110818558093",
+    "url": "/static/js/runtime-main.2a485af8.js"
+  }
+]);
