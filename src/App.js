71f585552d6eb2c2887ffc39582c89ffb5f1578e
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-04-29 14:34:07 -0300

    Add navigatior bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Home from './pages/Home';
+import Navigation from './components/Navigation';
 
 function App() {
   return(
     <BrowserRouter>
+      <Navigation />
       <Switch>
+
         <Route exact path="/" component={Home} />
       </Switch>
     </BrowserRouter>
---
+++ src/components/Navigation.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import styled from 'styled-components'
+import {Link } from 'react-router-dom'
+
+const Header = styled.header`
+  background-color: #fa163f;
+  padding:30px 0;
+`
+
+const NavContainer = styled.div`
+  width: 90%;
+  max-width:1400px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 18px;
+
+  h1 {
+    font-size: 21px;
+    text-transform: uppercase;
+    font-weight: bold;
+    letter-spacing: 0.25rem;
+    color: #fff;
+  }
+`
+
+const Nav = styled.nav`
+
+  ul{
+    display: flex;
+    list-style: none;
+
+  }
+
+  li + li {
+    margin-left: 20px;
+  }
+
+  a {
+    text-decoration: none;
+    text-transform: uppercase;
+    font-weight: 300;
+    cursor: pointer;
+    color: #fff;
+
+    &:hover {
+      color: #16FAD1;
+      font-weight: 400;
+    }
+  }
+`
+
+const Navigation = () => {
+  return (
+    <Header>
+      <NavContainer>
+        <h1>Movie-App</h1>
+        <Nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><a href="#">df</a></li>
+            <li><a href="#">df</a></li>
+          </ul>
+        </Nav>
+      </NavContainer>
+    </Header>
+  )
+}
+
+export default Navigation
